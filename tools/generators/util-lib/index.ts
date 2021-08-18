51cb31df4dda8817dfeb41c0754fd24162e24cb3
phattran <phattran@cisco.com>
2021-08-18 10:52:48 -0700

    feat: lab 13

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,6 +1,11 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
+interface Schema {
+  name: string;
+  directory: 'store' | 'api' | 'shared';
+}
+
 export default async function (tree: Tree, schema: any) {
   await libraryGenerator(tree, { name: `util-${schema.name}`, directory: schema.directory, tags: `type:util, scope:${schema.directory}` });
   await formatFiles(tree);
